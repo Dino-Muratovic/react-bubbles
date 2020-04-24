da5582e7bdc04c3047bb82ceafdc54decb19f2ce
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-04-24 13:44:12 -0500

    Created Protected route.js and axiosWithAuth for token. Working on creating a form and wiring it together

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,10 +4,10 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
       
-    </>
+    </div>
   );
 };
 
